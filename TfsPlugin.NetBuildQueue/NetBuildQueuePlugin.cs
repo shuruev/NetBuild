9665b7cee661dcc263f7d5e0f7828c1a811c51d4
Oleg Shuruev <shuruev@gmail.com>
2017-01-22 13:56:28 -0800

    Continue working on TFS plugin

---
+++ TfsPlugin.NetBuildQueue/NetBuildQueuePlugin.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.TeamFoundation.Common;
+using Microsoft.TeamFoundation.Framework.Server;
+using Microsoft.TeamFoundation.VersionControl.Server;
+
+namespace TfsPlugin.NetBuildQueue
+{
+	public class NetBuildQueuePlugin : ISubscriber
+	{
+		public string Name => "TfsPlugin.NetBuildQueue";
+		public SubscriberPriority Priority => SubscriberPriority.Normal;
+		public Type[] SubscribedTypes() => new[] { typeof(CheckinNotification) };
+
+		public EventNotificationStatus ProcessEvent(
+			TeamFoundationRequestContext requestContext,
+			NotificationType notificationType,
+			object notificationEventArgs,
+			out int statusCode,
+			out string statusMessage,
+			out ExceptionPropertyCollection properties)
+		{
+			statusCode = 0;
+			statusMessage = String.Empty;
+			properties = null;
+
+			try
+			{
+				if (notificationType == NotificationType.Notification)
+				{
+					var args = notificationEventArgs as CheckinNotification;
+					if (args != null)
+					{
+						ProcessCheckinEvent(args);
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				TeamFoundationApplicationCore.LogException($"An error occured in {Name}.", e);
+			}
+
+			return EventNotificationStatus.ActionPermitted;
+		}
+
+		private void ProcessCheckinEvent(CheckinNotification args)
+		{
+			TeamFoundationApplicationCore.Log("Hello world", 2505, EventLogEntryType.Information);
+			TeamFoundationApplicationCore.Log($"{args.ChangesetOwner.DisplayName} committed changeset #{args.Changeset}.", 2505, EventLogEntryType.Information);
+		}
+	}
+}
--- TfsPlugin.NetBuildQueue/Properties/AssemblyInfo.cs
+++ TfsPlugin.NetBuildQueue/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -8,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("TfsPlugin.NetBuildQueue")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Oleg Shuruev")]
 [assembly: AssemblyProduct("TfsPlugin.NetBuildQueue")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Oleg Shuruev 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
