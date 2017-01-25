68aa481c826249717940b12d22f770b2ccfb155e
Oleg Shuruev <shuruev@gmail.com>
2017-01-25 14:00:02 -0800

    Supported encrypted passwords

--- CCNet.NetBuildQueue.Plugin/NetBuildQueue.cs
+++ CCNet.NetBuildQueue.Plugin/NetBuildQueue.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using Exortech.NetReflector;
+using NetBuild.Common;
 using NetBuild.Queue.Core;
 using ThoughtWorks.CruiseControl.Core;
 using ThoughtWorks.CruiseControl.Core.Util;
@@ -20,6 +22,12 @@ namespace CCNet.NetBuildQueue.Plugin
 		[ReflectorProperty("db")]
 		public string DbConnection { get; set; }
 
+		[ReflectorProperty("thumbprint", Required = false)]
+		public string SecurityThumbprint { get; set; }
+
+		[ReflectorProperty("password", Required = false)]
+		public string EncryptedPassword { get; set; }
+
 		private string m_itemCode;
 		private QueueDb m_db;
 
@@ -31,9 +39,11 @@ namespace CCNet.NetBuildQueue.Plugin
 			}
 
 			Log.Debug("[NETBUILD] Getting changes for 'm_itemCode'...");
+			var sw = Stopwatch.StartNew();
 			var modifications = m_db.ShouldBuild(m_itemCode).Select(Convert).ToArray();
+			sw.Stop();
 
-			Log.Debug($"{modifications.Length} change(s) found.");
+			Log.Debug($"{modifications.Length} change(s) found in {sw.ElapsedMilliseconds} ms.");
 			return modifications;
 		}
 
@@ -77,8 +87,7 @@ namespace CCNet.NetBuildQueue.Plugin
 		{
 			m_itemCode = String.IsNullOrEmpty(ItemName) ? projectName : ItemName;
 
-			Log.Debug("[NETBUILD] Creating database connection using configuration values...");
-			m_db = new QueueDb(DbConnection, TimeSpan.FromSeconds(15));
+			m_db = InitDb();
 
 			Log.Debug($"[NETBUILD] Submitting item '{m_itemCode}' into database...");
 			m_db.SubmitItem(m_itemCode);
@@ -89,6 +98,34 @@ namespace CCNet.NetBuildQueue.Plugin
 
 			Log.Debug($"[NETBUILD] Setting source control trigger for item '{m_itemCode}'...");
 			m_db.SetTriggers(m_itemCode, "SourcePath", triggers);
+		}
+
+		private QueueDb InitDb()
+		{
+			// will put into configuration once there is a need
+			var timeout = TimeSpan.FromSeconds(15);
+
+			Log.Debug("[NETBUILD] Creating database connection using configuration values...");
+			if (!DbConnection.Contains("{password}"))
+			{
+				// not using the encrypted password
+				return new QueueDb(DbConnection, timeout);
+			}
+
+			// using the encrypted password
+			Log.Debug($"[NETBUILD] Using certificate '{SecurityThumbprint}' to decrypt database password...");
+
+			if (String.IsNullOrEmpty(SecurityThumbprint))
+				throw new InvalidOperationException("Configuration parameter <thumbprint> should be defined if <db> is using encrypted '{password}' syntax.");
+
+			if (String.IsNullOrEmpty(EncryptedPassword))
+				throw new InvalidOperationException("Configuration parameter <password> should be defined if <db> is using encrypted '{password}' syntax.");
+
+			var secure = new LocalEncryptor(SecurityThumbprint);
+			var password = secure.DecryptUtf8(EncryptedPassword);
+			var connection = DbConnection.Replace("{password}", password);
+
+			return new QueueDb(connection, timeout);
 		}
 
 		public void Purge(IProject project)
