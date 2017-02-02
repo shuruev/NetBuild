dc2d336c19c6fce896fef850b1b679af27108e0c
Oleg Shuruev <shuruev@gmail.com>
2017-02-02 14:56:52 -0800

    Added API client for queue server

--- NetBuild.Queue.Client/QueueClient.cs
+++ NetBuild.Queue.Client/QueueClient.cs
@@ -1,15 +1,61 @@
-﻿namespace NetBuild.Queue.Client
+﻿using System.Collections.Generic;
+using NetBuild.Common;
+using NetBuild.Queue.Core;
+
+namespace NetBuild.Queue.Client
 {
+	/// <summary>
+	/// Provides client features of a build queue server.
+	/// </summary>
 	public class QueueClient : ApiClient
 	{
+		/// <summary>
+		/// Initializes a new instance.
+		/// </summary>
 		public QueueClient(string baseUrl)
 			: base(baseUrl)
 		{
 		}
 
-		public bool Test(string item)
+		/// <summary>
+		/// For a specified item, updates a complete set of its triggers of a given type.
+		/// </summary>
+		public void SetTriggers<T>(string item, IEnumerable<T> triggers) where T : ITrigger
 		{
-			return Execute<bool>(HttpGet("test"));
+		}
+
+		/// <summary>
+		/// Processes any external signal, which can trigger new builds.
+		/// Returns a list of items which were potentially affected by this change.
+		/// </summary>
+		public List<string> ProcessSignal<T>(T signal) where T : ISignal
+		{
+			return Execute<List<string>>(HttpPost("process")
+				.WithParam("signal", signal.GetType().Name)
+				.WithBody(signal));
+		}
+
+		/// <summary>
+		/// Checks whether specified item should be built, and returns all corresponding modifications related to this build.
+		/// </summary>
+		public List<Modification> ShouldBuild(string item)
+		{
+			return Execute<List<Modification>>(HttpGet("check")
+				.WithParam("item", item));
+		}
+
+		/// <summary>
+		/// Starts build process for specified item, marking all the current modifications with specified build code.
+		/// </summary>
+		public void StartBuild(string itemCode, string buildCode)
+		{
+		}
+
+		/// <summary>
+		/// Marks specified build as completed, removing all corresponding modifications and updating related timestamps.
+		/// </summary>
+		public void CompleteBuild(string itemCode, string buildCode)
+		{
 		}
 	}
 }
